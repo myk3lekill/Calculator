8cc6e153ddb4b1822db91cfb8bfe4d0e5146a593
me <me@gmail.com>
2023-09-26 18:15:24 +0200

    sixth commit: JS Reset Value, Add Decimal

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 // DOM
 const calculatorDisplay = document.querySelector('h1');
 const inputBtns = document.querySelectorAll('button'); //this return an array of buttons
-const clearBtn = document.getElementById('clear.btn');
+const clearBtn = document.getElementById('clear-btn');
 
-// Send Numbers
+// Send Number Value
 function sendNumberValue(number) {
     // If current display value is 0, replace it, if not add number
     const displayValue = calculatorDisplay.textContent;
     calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
 }
 
+// Clear
+function resetAll() {
+    calculatorDisplay.textContent = '0'
+}
+
+// Add Decimal
+function addDecimal() {
+    // If no decimal, add one
+    if (!calculatorDisplay.textContent.includes('.')) {
+        calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+    }
+}
 
 // Event Listener for Numbers Operator and Decimal buttons
 inputBtns.forEach((inputBtn) => {
@@ -18,6 +30,10 @@ inputBtns.forEach((inputBtn) => {
     } else if (inputBtn.classList.contains('operator')) {
         inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
     } else if (inputBtn.classList.contains('decimal')){
-        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+        inputBtn.addEventListener('click', () => addDecimal(inputBtn.value));
     }
 });
+
+// Event Listener for Clear button
+clearBtn.addEventListener('click', resetAll)
+
